097cc4871c3e5e0f92c0b29bc110adc7ab350bb9
Albert Tanure <tanure@live.com>
2017-04-22 09:38:41 -0300

    Configurando ambiente

--- AzureBootcampBH/Controllers/HomeController.cs
+++ AzureBootcampBH/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +11,8 @@ namespace AzureBootcampBH.Controllers
     {
         public ActionResult Index()
         {
-            throw new Exception("Erro forçado");
+            //throw new Exception("Erro forçado");
+            ViewBag.Ambiente = ConfigurationManager.AppSettings["Ambiente"];
             return View();
         }
 
